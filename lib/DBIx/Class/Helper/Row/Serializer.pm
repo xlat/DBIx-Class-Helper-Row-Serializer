6bc8eab124864ad2466111628eff26d3639fed5f
Nicolas GEORGES <nicolas54.georges@gmail.com>
2015-08-07 18:27:00 +0200

    fix Helper::Row::JSON incompatibility

--- lib/DBIx/Class/Helper/Row/Serializer.pm
+++ lib/DBIx/Class/Helper/Row/Serializer.pm
@@ -7,16 +7,16 @@ use parent 'DBIx::Class::Row';
 # 'is_serializable' may conflict with DBIx::Class::Helper::Row::ToJSON
 my $IS_SERIALISABLE  = 'is_serializable';
 
-__PACKAGE__->mk_group_accessors(inherited => '__serializable_columns');
-__PACKAGE__->mk_group_accessors(inherited => '__serializable_relationships');
+__PACKAGE__->mk_group_accessors(inherited => '_hrs_serializable_columns');
+__PACKAGE__->mk_group_accessors(inherited => '_hrs_serializable_relationships');
 
-sub __is_column_serializable {
+sub hrs_is_column_serializable {
     my ( $self, $column ) = @_;
     my $info = $self->column_info($column);
     if (!defined $info->{$IS_SERIALISABLE}) {
         #ignore autoincrement columns
     if (exists $info->{is_auto_increment} &&
-            $info->{$info->{is_auto_increment}}
+            $info->{is_auto_increment}
         ) {
             $info->{$IS_SERIALISABLE} = 0;
         } else {
@@ -26,27 +26,26 @@ sub __is_column_serializable {
     return $info->{$IS_SERIALISABLE};
 }
  
-sub _serializable_columns {
+sub hrs_serializable_columns {
     my $self = shift;
-    if (!$self->__serializable_columns) {
+    if (!$self->_hrs_serializable_columns) {
         my $rsrc = $self->result_source;
         my %fk_columns = 
             map { %{  $_->{attrs}{fk_columns}  } }
             grep{ exists $_->{attrs}{fk_columns} }
             map { $rsrc->relationship_info($_)   }
             $rsrc->relationships;
-		$DB::single=1;
-        $self->__serializable_columns([
-            grep $self->__is_column_serializable($_),
+        $self->_hrs_serializable_columns([
+            grep $self->hrs_is_column_serializable($_),
             #ignore fkeys by defaults
             grep { !exists $fk_columns{$_} }
                 $self->result_source->columns
         ]);
    }
-   return $self->__serializable_columns;
+   return $self->_hrs_serializable_columns;
 }
 
-sub __is_relationship_serializable {
+sub hrs_is_relationship_serializable {
     my ( $self, $rel_name ) = @_;
     
     my $rsrc = $self->result_source;
@@ -60,29 +59,23 @@ sub __is_relationship_serializable {
     return $rel_info->{attrs}{$IS_SERIALISABLE};
 }
 
-sub _serializable_relationships {
+sub hrs_serializable_relationships {
     my $self = shift;
-    if (!$self->__serializable_relationships) {
-        $self->__serializable_relationships([
-            grep $self->__is_relationship_serializable($_),
+    if (!$self->_hrs_serializable_relationships) {
+        $self->_hrs_serializable_relationships([
+            grep $self->hrs_is_relationship_serializable($_),
                 $self->result_source->relationships
         ]);
    }
-   return $self->__serializable_relationships;
+   return $self->_hrs_serializable_relationships;
 }
 
-=head2 serialize
-
-    $self->serialize
-    
-returns a serialisation of your object, typicaly a hashref.
-
-=cut
 sub serialize{
     my ($self, %args) = @_;
 	return unless $self->isa('DBIx::Class::Helper::Row::Serializer');
-    my $columns = $self->_serializable_columns;
-    my $columns_info = $self->columns_info(@$columns);
+	
+    my $columns = $self->hrs_serializable_columns;
+    my $columns_info = $self->columns_info($columns);
 	my $col_data = {
         map +($_ => $self->$_),
         map +($columns_info->{$_}{accessor} || $_),
@@ -90,11 +83,10 @@ sub serialize{
     };
 	 my $rel_names_copied = {};
      my $rsrc = $self->result_source;
-     my $relationships = $self->_serializable_relationships;
+     my $relationships = $self->hrs_serializable_relationships;
 	RELATIONSHIP:
 	foreach my $rel_name (@$relationships) {
 		my $rel_info = $rsrc->relationship_info($rel_name);
-		$DB::single = 1 unless $rel_info->{class};
 		my $serializer = exists $rel_info->{attrs}{serialize}
 						 ? $rel_info->{attrs}{serialize}
 						 : ( $rel_info->{class}->can('serialize')
